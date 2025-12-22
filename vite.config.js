fa1d928f8ef16fafb56e055175b820b4fe4dcd73
Shrondaj <shrjeacompany@gmail.com>
2025-12-21 22:37:38 -0700

    Fix vite config to use React plugin

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000,           // Custom port (change to whatever you want)
-    open: true,           // Auto-open browser when server starts
-    host: true,           // Listen on all network interfaces
-    strictPort: false,    // If port is in use, try the next available port
-  }
+  plugins: [react()],
+  base: '/scoopz-app/',
 })
